2a8b7d5242367e6be8510168e05361a32f5062b0
hakeem <belloharyourmidey@gmail.com>
2024-10-25 07:29:29 -0700

    final commit

--- useReducer/src/App.jsx
+++ useReducer/src/App.jsx
@@ -1,45 +1,46 @@
 import { useReducer } from 'react';
 import './App.css';
 
-const initialstate  = {
+const initialState  = {
   count: 0,
-  error: string | null, 
+  error: null, 
 }
-
-// const Action = {
-//   type: 'increment' | 'decrement'
-// }
 
 function reducer(state, action){
   switch(action.type){
     case 'increment': {
-      const newCount = count + 1;
-      const hasError = newCount > 5;
+      const newCount = state.count + 1;
+      const limit = newCount > 5;
 
-      return {...state, count: hasError ? state.count : newCount, 
-        error: hasError ? 'Maximum count reached ' : null,
+      return {...state, 
+        count: limit ? state.count : newCount, 
+        error: limit ? 'Maximum count reached ' : null,
       }
     }
     case 'decrement': {
-      const newCount = count - 1;
-      const hasError = newCount < 0;
+      const newCount = state.count - 1;
+      const limit = newCount < 0;
 
-      return {...state, count: hasError ? state.count : newCount, 
-        error: hasError ? 'Minimum count reached ' : null,
+      return {...state, 
+        count: limit ? state.count : newCount, 
+        error: limit ? 'Minimum count reached ' : null,
       }
     }
     default:
-      return initialstate;
+      return initialState;
   }
 }
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialstate);
-
-
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <>
-      
+      <div>
+        <h2>Count: {state.count}</h2>
+        {state.error && <div>{state.error}</div>}
+        <button onClick={() => dispatch({type: 'increment'})}>Increment</button>
+        <button onClick={() => dispatch({type: 'decrement'})}>Decrement</button>
+      </div>
     </>
   )
 }
